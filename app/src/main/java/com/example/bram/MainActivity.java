dbbba8b4aaa73d27bf9527dc73d28aa276bc2485
Atilla <oomenatilla@gmail.com>
2023-05-30 21:20:13 +0200

    lampje

--- app/src/main/java/com/example/bram/MainActivity.java
+++ app/src/main/java/com/example/bram/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.bram;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+    private Switch switchButton;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        switchButton = findViewById(R.id.switch_button);
+        imageView = findViewById(R.id.image_view);
+
+        switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    imageView.setImageResource(R.drawable.lamp_aan); // Set the new image resource when the switch is checked
+                } else {
+                    imageView.setImageResource(R.drawable.lamp_uit); // Set the default image resource when the switch is unchecked
+                }
+            }
+        });
     }
 }
